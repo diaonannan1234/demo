191dece2f6da30f98fe5426d007771e0a39292b4
du_wei <duwais@126.com>
2022-08-09 09:56:15 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM java:8-jdk-alpine
+
+COPY ./demo-0.0.1-SNAPSHOT.jar /usr/app/
+
+WORKDIR /usr/app
+
+
+ENTRYPOINT ["java", "-jar", "demo-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/duwei/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.duwei.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String port;
+    @GetMapping("/")
+    public String hello(){
+        return appName+":"+port;
+    }
+}
